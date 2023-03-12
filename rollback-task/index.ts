ae59893913b3f007649281a85022b70b957f6c53
Yasen <ydinkov@gmail.com>
2023-03-12 10:42:57 +0100

    Added some more logging

--- rollback-task/index.ts
+++ rollback-task/index.ts
@@ -1,49 +1,55 @@
 import tl = require('azure-pipelines-task-lib/task');
 import https from 'https';
 
+const OrganizationUri = tl.getVariable("System.CollectionUri");
+    console.log(`Reading OrganizationUri:${OrganizationUri}`);
+const Project = tl.getVariable("System.TeamProject");
+    console.log(`Reading Project:${Project}`);
+const Token = tl.getVariable("System.AccessToken");  
+    console.log(`Reading Token:${"*".repeat(Token.length)}`);
+const DefinitionId = tl.getVariable("System.DefinitionId");
+    console.log(`Reading DefinitionId:${DefinitionId}`);
+const BuildId = tl.getVariable("Build.BuildId");
+    console.log(`Reading BuildId:${BuildId}`);
+const Stage =  tl.getVariable("System.StageName");
+    console.log(`Reading Stage:${Stage}`);
+const BranchFilter = tl.getInput("BranchFilter");
+    console.log(`Reading BranchFilter:${BranchFilter}`);
+
+
+const baseUrl = `${OrganizationUri}/${Project}/_apis`;       
+const auth = Buffer.from(`:${Token}`).toString('base64');
+
+const PatchPayload = JSON.stringify({
+    state: 1,
+    forceRetryAllJobs: true
+});
+
+const GetOptions = {
+    method: 'GET',
+        headers: {
+            Authorization: `Basic ${auth}`,
+        },
+    };
+
+    const PatchOptions = {
+    method: 'PATCH',
+    headers: {
+        Authorization: `Basic ${auth}`,                
+        'Content-Type': 'application/json'
+    },
+};
+        
+
 async function run() {
-    try {
-        ///const inputString: string | undefined = tl.getInput('samplestring', true);
-        const OrganizationUri = tl.getVariable("System.CollectionUri");
-        const Project = tl.getVariable("System.TeamProject");
-        const Token = tl.getVariable("System.AccessToken");
-        const DefinitionId = tl.getVariable("System.DefinitionId");
-        const BuildId = tl.getVariable("Build.BuildId");
-        const Stage =  tl.getVariable("System.StageName");
-        const BranchFilter = tl.getInput("BranchFilter");        
+    try {        
+        console.log("Starting rollback")
 
-
-        const baseUrl = `${OrganizationUri}/${Project}/_apis`;       
-        const auth = Buffer.from(`:${Token}`).toString('base64');
-  
-        const PatchPayload = JSON.stringify({
-            state: 1,
-            forceRetryAllJobs: true
-        });
-
-        let GetOptions = {
-            method: 'GET',
-            headers: {
-              Authorization: `Basic ${auth}`,
-            },
-          };
-        
-        let PatchOptions = {
-            method: 'PATCH',
-            headers: {
-                Authorization: `Basic ${auth}`,                
-                'Content-Type': 'application/json'
-            },
-        };
-        
-        console.log("Init Complete")
         let buildsUrl = `${baseUrl}/build/builds?definitions=${DefinitionId}&api-version=7.0`;
         let req = https.request(buildsUrl, GetOptions, (res) => {
             console.log("Retreiving previous builds")
             let data = '';
-            res.on('data', (chunk) => {
-                data += chunk;
-            });
+            res.on('data', (chunk) => { data += chunk; });
             
             res.on('end', () => {
                 let response = JSON.parse(data);
@@ -51,23 +57,19 @@ async function run() {
 
                 let LatestSuccessfulBuildId = response.value
                     .filter((run: Record<string, any>) => run.id < Number(BuildId) && run.status === "completed" && run.result === "succeeded" && run.sourceBranch === BranchFilter)
-                    .map((run: Record<string, any>) => ({
-                        id: run.id,
-                    }))
+                    .map((run: Record<string, any>) => ({id: run.id}))
                     .sort((a: Record<string, any>, b: Record<string, any>) => b.id - a.id)[0].id;
 
                 console.log(`Rolling back from  ${BuildId}:${Stage} to -> ${LatestSuccessfulBuildId}:${Stage}`)   
-                //Now Execute
+
+                
                 let redeployUrl =  `${baseUrl}/build/builds/${LatestSuccessfulBuildId}/stages/${Stage}?api-version=7.0` 
                 let req = https.request(redeployUrl, PatchOptions, (res) => {
                     let responseData = '';            
                     console.log(res.statusCode)
-                    res.on('data', (chunk) => {
-                        responseData += chunk;
-                    });
-                
+                    res.on('data', (chunk) => {responseData += chunk;});                
                     res.on('end', () => {
-                        //let response = JSON.parse(data);                         
+                        //let response = JSON.parse(data);
                         console.log("Patch complete")
                         console.log(responseData)
                     });
